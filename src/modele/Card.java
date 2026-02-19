69a05f1c4d5e9dd1b38ab641de62b076d6fa9391
Kelpie <alex.caffarel@gmail.com>
2026-02-19 12:31:13 +0100

    Added attributes and constructor to Card

--- src/modele/Card.java
+++ src/modele/Card.java
@@ -1,5 +1,35 @@
 package modele;
 
 public class Card {
+	public enum Type {RED,WHITE,GREEN,BLUE,DARK,NONE};
+	private Type color;
+	private int totalCost;
+	private int colorCost;
+	private int atk;
+	private int def;
+	private boolean land;
+	private String name;
+	private String description;
+	private String[] skills;
+	private boolean tapped;
 	
+	
+	/**
+	 * This constructor is used to create any card
+	*/
+	public Card( Type color, int totalcost, int colorcost, int attack, int defense, boolean land, String nom, String[] bonus ) {
+		this.color = color;
+		this.land = land;
+		totalCost = totalcost;
+		colorCost = colorcost;
+		atk = attack;
+		def = defense;
+		name = nom;
+		tapped = false;
+		if (land) {			
+			description = "Terrain";			
+		} else {
+			description = "Créature";
+		}
+	}
 }
